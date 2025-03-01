3aac8291e17e34792dfbe8cca3ced90a0c2bdf0d
Ira Gordon IV <ig4@icode4food.org>
2025-03-01 13:33:45 -0600

    create functions that return values

--- investment_calculator.go
+++ investment_calculator.go
@@ -5,8 +5,11 @@ import (
 	"math"
 )
 
+// create global variable
+const inflationRate = 3.5
+
 func main() {
-	const inflationRate = 3.5
+
 	var investmentAmount float64
 	var years float64
 	expectedReturnRate := 5.5
@@ -21,8 +24,9 @@ func main() {
 	fmt.Print("Enter years: ")
 	fmt.Scan(&years)
 
-	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
-	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
+	futureValue, futureRealValue := calculateFutureValue(investmentAmount, expectedReturnRate, years)
+	// futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	// futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
 
 	//outputs to standard output
 	fmt.Println("Future Value:", futureValue)
@@ -41,4 +45,11 @@ func main() {
 // create a function example
 func outputText(text string) {
 	fmt.Print(text)
+}
+
+// create a function that returns a value
+func calculateFutureValue(investmentAmount float64, expectedReturnRate float64, years float64) (float64, float64) {
+	fv := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	rfv := fv / math.Pow(1+inflationRate/100, years)
+	return fv, rfv
 }
