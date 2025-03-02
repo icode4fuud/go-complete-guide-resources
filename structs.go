efd2485f6e1039007bd4a00ffa703d3d92cf9134
icode4fuud <ig4@icode4food.org>
2025-03-02 11:18:43 -0600

    Bind a method to a struct by creating a reciever method

--- structs.go
+++ structs.go
@@ -13,6 +13,18 @@ type user struct {
 	createdAt time.Time
 }
 
+// now attach outputUserDetails to the user struct
+// u user is a receiver argument
+// func (u *user) outputUserDetails() { //<= can use a pointer here as the pointer for more efficiency
+func (u user) outputUserDetails() {
+	//dereference to perform data mutation
+	// (*u).firstName = "John"
+	// (*u).lastName = "Doe"
+	// (*u).birthDate = "01/01/1970"
+
+	fmt.Println(u.firstName, u.lastName, u.birthDate)
+}
+
 func main() {
 	userFirstName := getUserData("Please enter your first name: ")
 	userLastName := getUserData("Please enter your last name: ")
@@ -27,16 +39,9 @@ func main() {
 
 	// ... do something awesome with that gathered data!
 	// now passing a pointer instead of an unnecessary copy
-	outputUserDetails(&appUser)
-}
-
-func outputUserDetails(u *user) { //<= can use a pointer here as the pointer for more efficiency
-	//dereference to perform data mutation
-	(*u).firstName = "John"
-	(*u).lastName = "Doe"
-	(*u).birthDate = "01/01/1970"
-
-	fmt.Println(u.firstName, u.lastName, u.birthDate)
+	//outputUserDetails(&appUser)
+	// now using the receiver method w/o any arguments
+	appUser.outputUserDetails()
 }
 
 func getUserData(promptText string) string {
