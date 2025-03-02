41ea187459df7b0c4eb52500f7f9000440c78c5c
icode4fuud <ig4@icode4food.org>
2025-03-02 07:54:53 -0600

    Added For Looping

--- ifelse/bank.go
+++ ifelse/bank.go
@@ -48,36 +48,57 @@ func main() {
 	}
 
 	fmt.Println("Welcome to Go Bank!!!")
-	fmt.Println("What would you like to do today?")
-	fmt.Println("1. Check Balance")
-	fmt.Println("2. Deposit Money")
-	fmt.Println("3. Withdraw Money")
-	fmt.Println("4. Exit")
 
-	var choice int
-	fmt.Print("Your choice: ")
-	fmt.Scanln(&choice)
+	for { //i := 0; i < count; i++
 
-	wantToCheckBalaaance := choice == 1
+		fmt.Println("What would you like to do today?")
+		fmt.Println("1. Check Balance")
+		fmt.Println("2. Deposit Money")
+		fmt.Println("3. Withdraw Money")
+		fmt.Println("4. Exit")
 
-	//replace w/ switch
-	if wantToCheckBalaaance {
-		fmt.Println("Your balance is:", accountBalance)
-	} else if choice == 2 {
-		fmt.Println("Enter the amount you want to deposit:")
-		var depositAmount float64
-		fmt.Scanln(&depositAmount)
-		accountBalance += depositAmount
-		fmt.Println("Your new balance is:", accountBalance)
-		writeBalanceToFile(accountBalance)
-	} else if choice == 3 {
-		fmt.Println("Enter the amount you want to withdraw:")
-		var withdrawAmount float64
-		fmt.Scanln(&withdrawAmount)
+		var choice int
+		fmt.Print("Your choice: ")
+		fmt.Scanln(&choice)
 
-	} else {
-		fmt.Println("Thank you for using Go Bank!!!")
+		wantToCheckBalaaance := choice == 1
+
+		//replace w/ switch
+		if wantToCheckBalaaance {
+			fmt.Println("Your balance is:", accountBalance)
+		} else if choice == 2 {
+			fmt.Println("Enter the amount you want to deposit:")
+			var depositAmount float64
+			fmt.Scanln(&depositAmount)
+
+			//check if the user enters invalid amount
+			if depositAmount <= 0 {
+				fmt.Println("Invalid amount. Must be greater than 0")
+				continue
+			}
+
+			accountBalance += depositAmount
+			fmt.Println("Your new balance is:", accountBalance)
+			writeBalanceToFile(accountBalance)
+		} else if choice == 3 {
+			fmt.Println("Enter the amount you want to withdraw:")
+			var withdrawAmount float64
+			fmt.Scanln(&withdrawAmount)
+
+			//check if the user enters invalid amount
+			if withdrawAmount <= 0 {
+				fmt.Println("Invalid amount. Must be greater than 0")
+				continue
+			}
+
+			accountBalance -= withdrawAmount
+			fmt.Println("Your new balance is:", accountBalance)
+			writeBalanceToFile(accountBalance)
+
+		} else {
+			fmt.Println("Thank you for using Go Bank!!!")
+			break
+		}
 	}
 
-	//fmt.Println(("Your choice:"), choice)
 }
