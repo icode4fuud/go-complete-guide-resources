1b1f9ab2d25d9e8995e39d5b5371c28ff87f3f9c
icode4fuud <ig4@icode4food.org>
2025-03-02 09:11:01 -0600

    Learned packaging by splitting code across files

---
+++ home_screen_options.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+//by added this file to the main package it can be referenced in all files in the main package
+func presentOptions() {
+	fmt.Println("What do you want to do?")
+	fmt.Println("1. Check balance")
+	fmt.Println("2. Deposit money")
+	fmt.Println("3. Withdraw money")
+	fmt.Println("4. Exit")
+}
