da85a4f1d537b8f4c25f0b3122a6b27b969bbe73
icode4fuud <ig4@icode4food.org>
2026-02-10 10:50:55 -0600

    server init

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	server := gin.Default()
+
+	//handler for http request
+	server.GET("/events", getEvents) //GET, POST, PUT, PATCH, DELETE
+
+	server.Run(":8081") // localhost:8081
+}
+
+func getEvents(context *gin.Context) {
+	context.JSON(http.StatusOK, gin.H{"message": "Hello World!"})
 }
