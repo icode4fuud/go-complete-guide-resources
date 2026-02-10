9fb63e3be2ceb572dbd0967edb67e2bbb659ef94
icode4fuud <ig4@icode4food.org>
2026-02-10 12:32:39 -0600

    functioning server with events, tournaments

--- main.go
+++ main.go
@@ -4,17 +4,53 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"ig4llc.com/models"
 )
 
 func main() {
 	server := gin.Default()
 
-	//handler for http request
+	//register endpoints as a handler for http request
 	server.GET("/events", getEvents) //GET, POST, PUT, PATCH, DELETE
+	server.POST("/events", createEvent)
 
 	server.Run(":8081") // localhost:8081
 }
 
 func getEvents(context *gin.Context) {
-	context.JSON(http.StatusOK, gin.H{"message": "Hello World!"})
+
+	events := models.GetAllEvents()
+
+	context.JSON(http.StatusOK, events)
 }
+
+func createEvent(context *gin.Context) {
+	var event models.Event
+	err := context.ShouldBindJSON(&event)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse data"}) //err.Error()
+		return
+	}
+
+	event.ID = 1
+	event.UserID = 1
+	event.Save()
+	context.JSON(http.StatusCreated, gin.H{"message": "Event created", "event": event})
+
+	//
+	// context.JSON(http.StatusCreated, event)
+	// context.ShouldBindJSON(&event)
+}
+
+// func updateEvent(context *gin.Context) {
+
+// }
+
+// func deleteEvent(context *gin.Context) {
+
+// }
+
+// func getEvent(context *gin.Context) {
+
+// }
